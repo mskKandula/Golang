5583520f35cfc362b6148f9c1773e81a23dcd6d4
mskKandula <mskKandula@gmail.com>
2021-01-27 14:03:56 +0530

    Implemented Jwt Decode Function

--- JWT/main.go
+++ JWT/main.go
@@ -38,6 +38,36 @@ func generateJwt() (string, time.Time, error) {
 	return tokenString, expirationTime, nil
 }
 
+func decodeJwt(tokenString string) (interface{}, error) {
+
+	// Initialize a new instance of `Claims`
+	claims := jwt.MapClaims{}
+
+	// Parse the JWT string and store the result in `claims`.
+	// Note that we are passing the key in this method as well. This method will return an error
+	// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	// or if the signature does not match
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("jwtKey")), nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return "", err
+		}
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", err
+	}
+
+	// can get the other values as well
+	email := claims["email"]
+
+	return email, nil
+}
+
 func main() {
 
 	tokenStr, expirationTime, err := generateJwt()
@@ -50,4 +80,13 @@ func main() {
 
 	fmt.Println("The token will expire at : ", expirationTime)
 
+	email, err := decodeJwt(tokenStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Perform Authorization or Authentication based on the email & password from decoded token
+
+	fmt.Println("Email is :", email)
 }
