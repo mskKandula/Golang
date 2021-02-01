0bf542f6c1d635e348243c485db7a366a6bef88d
mskKandula <mskKandula@gmail.com>
2021-02-01 13:17:47 +0530

    Implemented Signup Controller

--- WebApp/Server/controller/controller.go
+++ WebApp/Server/controller/controller.go
@@ -1,8 +1,35 @@
 package controller
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/mskKandula/model"
+)
+
+var (
+	Db  *sql.DB
+	err error
+)
 
 func Signup(w http.ResponseWriter, r *http.Request) {
+
+	var user model.User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	query, err := Db.Prepare("INSERT INTO USERS(name, age, email, phoneNo, password) VALUES(?,?,?,?,?)")
+	if err != nil {
+		log.Panic(err)
+	}
+	query.Exec(user.Name, user.Age, user.Email, user.PhoneNo, user.Password)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(user)
 
 }
 func Login(w http.ResponseWriter, r *http.Request) {
