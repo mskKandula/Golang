3e70269dce68ca4fb11e0c8144356367c91c369e
mskKandula <mskKandula@gmail.com>
2021-02-03 14:30:58 +0530

    Implemented JWT Decode in Middleware

--- WebApp/Server/middleware/middleware.go
+++ WebApp/Server/middleware/middleware.go
@@ -27,14 +27,42 @@ func Auth(creds model.Validation) (string, time.Time, error) {
 
 	atClaims["expireAt"] = expirationTime
 
-	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 
-	token, err := at.SignedString([]byte(os.Getenv("jwtKey")))
+	tokenString, err := token.SignedString([]byte(os.Getenv("jwtKey")))
 
 	if err != nil {
 		return "", expirationTime, err
 	}
 
-	return token, expirationTime, nil
+	return tokenString, expirationTime, nil
 
+}
+
+func Decode(tokenString string) (interface{}, error) {
+
+	// Initialize a new instance of `Claims`
+	claims := jwt.MapClaims{}
+	// Parse the JWT string and store the result in `claims`.
+	// Note that we are passing the key in this method as well. This method will return an error
+	// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	// or if the signature does not match
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("jwtKey")), nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return "", err
+		}
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", err
+	}
+
+	email := claims["email"]
+
+	return email, nil
 }
