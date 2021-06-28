aeafd1fc59fed2c6f0ef413dc20192e9408bb99d
mskKandula <mskKandula@gmail.com>
2021-06-28 10:46:59 +0530

    Http to websocket conn

--- WebSockets/V2/go.mod
+++ WebSockets/V2/go.mod
@@ -1,3 +1,8 @@
 module github.com/mskKandula
 
 go 1.14
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+)
--- WebSockets/V2/main.go
+++ WebSockets/V2/main.go
@@ -4,16 +4,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+// Client represents the websocket client at the server
+type Client struct {
+	// The actual websocket connection.
+	conn *websocket.Conn
+}
+
+func newClient(conn *websocket.Conn) *Client {
+	return &Client{
+		conn: conn,
+	}
+}
 
 func main() {
 	fmt.Println("Hello")
 
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", ServeWs)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
+}
+
+// ServeWs handles websocket requests from clients requests.
+func ServeWs(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := newClient(conn)
+
+	fmt.Println("New Client Joined")
+
+	fmt.Println("client : ", client)
 }
