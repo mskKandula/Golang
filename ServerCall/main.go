a535bdf81a5fe688d2f83aa34bf3b558b6de13ee
mskKandula <mskKandula@gmail.com>
2021-07-09 20:40:35 +0530

    Server Call

---
+++ ServerCall/main.go
@@ -0,0 +1,57 @@
+package main
+
+//added
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/tidwall/sjson"
+)
+
+const (
+	Url string = "http://1232.com"
+)
+
+func main() {
+
+	fmt.Println("Hello")
+
+	LearnerCodeList := [][]string{{"123", "546", "789"}, {"546", "342", "265"}}
+
+	CenterCodeList := []string{"4352r435refr", "654t53482"}
+
+	for index, CenterCode := range CenterCodeList {
+
+		requestObj := `{}`
+
+		requestObj, _ = sjson.Set(requestObj, CenterCode, LearnerCodeList[index])
+
+		req, err := http.NewRequest("POST", Url, bytes.NewBuffer([]byte(requestObj)))
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println("response Body:", string(body))
+
+		//Prepare the data
+
+		//Service call to store in DB
+	}
+}
