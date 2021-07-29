4868ee269043eb6148af41540463553b3715711f
mskKandula <mskKandula@gmail.com>
2021-07-29 14:07:15 +0530

    Template Data handler

---
+++ TemplateExecution/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Institute struct {
+	InstituteName      string    `json:"instituteName"`
+	RepresentativeName string    `json:"representativeName"`
+	LearnerList        []Learner `json:"learnerList"`
+	InstituteUrl       string    `json:"instituteUrl"`
+}
+
+type Learner struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Standard  string `json:"standard"`
+	Division  string `json:"division"`
+	LoginName string `json:"loginName"`
+}
+
+func templateHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("./SendEmailToInstitute.html")
+
+	data := Institute{
+		InstituteName:      "ABC",
+		RepresentativeName: "XYZ",
+		InstituteUrl:       "https://example.com",
+		LearnerList: []Learner{
+			{FirstName: "Learner", LastName: "One", Standard: "10", Division: "A", LoginName: "learnero"},
+			{FirstName: "Learner", LastName: "Two", Standard: "9", Division: "A", LoginName: "learnert"},
+			{FirstName: "Learner", LastName: "Three", Standard: "8", Division: "A", LoginName: "learnert1"},
+		},
+	}
+
+	tmpl.Execute(w, data)
+
+}
+func main() {
+	http.HandleFunc("/", templateHandler)
+	http.ListenAndServe(":8081", nil)
+}
