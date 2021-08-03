e0940df9e52281c9c741936ae69adf4616321301
mskKandula <mskKandula@gmail.com>
2021-08-03 14:42:17 +0530

    Write to & Read from file

--- ProtoBuf/main.go
+++ ProtoBuf/main.go
@@ -2,15 +2,53 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/mskKandula/protobuf/personDetails"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
 	person := getDetails()
 
-	fmt.Println("The Details of a person are : ", person)
+	writeToFile(person, "person.bin")
+	readFromFile("person.bin")
 
+}
+
+func readFromFile(fname string) error {
+	out, err := ioutil.ReadFile(fname)
+
+	if err != nil {
+		log.Fatalln("Unable to read Person file: ", err)
+		return err
+	}
+
+	person := &personDetails.Person{}
+
+	if err = proto.Unmarshal(out, person); err != nil {
+		log.Fatalln("Unable to read Person from a file : ", err)
+		return err
+	}
+	fmt.Println("Successfully read a person file: ", person)
+	return nil
+}
+
+func writeToFile(person *personDetails.Person, fname string) error {
+	out, err := proto.Marshal(person)
+
+	if err != nil {
+		log.Fatalln("Unable to Marshal Person: ", err)
+		return err
+	}
+
+	if err = ioutil.WriteFile(fname, out, 0666); err != nil {
+		log.Fatalln("Unable to write Person to a file: ", err)
+		return err
+	}
+	fmt.Println("Successfully Written")
+	return nil
 }
 
 func getDetails() *personDetails.Person {
