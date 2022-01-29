678b25f1c7bef2bf36a9600a3a021d9551515989
mskKandula <mskKandula@gmail.com>
2022-01-29 16:37:53 +0530

    Products Filtration

---
+++ Interface/products.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type Color int
+type Size int
+
+const (
+	small Size = iota
+	medium
+	large
+)
+
+const (
+	red Color = iota
+	blue
+	green
+)
+
+type Product struct {
+	name  string
+	color Color
+	size  Size
+}
+
+type Filter struct {
+}
+
+func (f *Filter) FilterByColor(Products []Product, color Color) []*Product {
+	result := make([]*Product, 0)
+
+	for i, product := range Products {
+		if product.color == color {
+			result = append(result, &Products[i])
+
+		}
+	}
+	return result
+}
+
+func main() {
+	var f Filter
+	products := []Product{
+		{"apple", green, small},
+		{"apple", red, small},
+		{"Watermelon", green, large},
+	}
+
+	for _, product := range f.FilterByColor(products, green) {
+		fmt.Printf("-Green %s\n", product.name)
+	}
+
+}
