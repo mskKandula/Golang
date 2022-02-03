a6a1042f4725a4ceef8eb6013fd27def14bb2e15
mskKandula <mskKandula@gmail.com>
2022-02-03 11:33:55 +0530

    Synchronous

--- DataPipeline/main.go
+++ DataPipeline/main.go
@@ -5,15 +5,55 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/tidwall/gjson"
 )
 
 const url = "https://jsonplaceholder.typicode.com/"
 
 func main() {
-	FetchUsers(url)
+	t := time.Now()
+	users := FetchUsers(url)
+
+	for _, user := range users.Array() {
+		userId := user.Get("id").Int()
+
+		posts := FetchPosts(url, int(userId))
+
+		for _, post := range posts.Array() {
+			fmt.Println(post)
+		}
+	}
+	fmt.Println(time.Since(t))
 }
 
-func FetchUsers(url string) {
+func FetchPosts(url string, userId int) gjson.Result {
+
+	postsUrl := url + "posts?userId=" + strconv.Itoa(userId)
+
+	resp, err := http.Get(postsUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := gjson.ParseBytes(body)
+
+	return result
+
+}
+
+func FetchUsers(url string) gjson.Result {
 
 	userUrl := url + "users"
 
@@ -31,6 +71,8 @@ func FetchUsers(url string) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
+	result := gjson.ParseBytes(body)
+
+	return result
 
 }
