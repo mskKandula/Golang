2e082003cdba4e934bcc08b749a13224b186a6d9
mskKandula <mskKandula@gmail.com>
2022-02-15 17:27:23 +0530

    Implemented ZipFile Download HandleFunc

--- Responses/main.go
+++ Responses/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -9,6 +11,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
@@ -21,12 +25,19 @@ type User struct {
 	Phone string `json:"phone"`
 }
 
+var (
+	imagePathChannel        = make(chan string, 500)
+	rootPath         string = "../ProcessingPipeline/ImageProcessing/Images/"
+	// files            []string
+)
+
 func main() {
 	http.HandleFunc("/", stringHandler)
 	http.HandleFunc("/json", jsonHandler)
 	http.HandleFunc("/jsonVariant", jsonVariantHandler)
 	http.HandleFunc("/template", templateHandler)
 	http.HandleFunc("/file", fileHandler)
+	http.HandleFunc("/zipFile", zipFileHandler)
 	fmt.Println("listening on port 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
@@ -97,7 +108,7 @@ func prepareResult(keys []string, vals []interface{}) gjson.Result {
 // fileHandler downloads a file on the client machine
 func fileHandler(w http.ResponseWriter, r *http.Request) {
 
-	imageFile, er := os.Open("./Sample.png")
+	imageFile, err := os.Open("./Sample.png")
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -129,4 +140,70 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {
 		Email: "YourEmail",
 		Phone: "YourNumber"}
 	t.Execute(w, user)
+}
+
+func zipFileHandler(w http.ResponseWriter, r *http.Request) {
+
+	filename := "ImageFiles"
+
+	buf := new(bytes.Buffer)
+
+	zipWriter := zip.NewWriter(buf)
+
+	go func() {
+		defer close(imagePathChannel)
+		if err := filepath.Walk(rootPath, visit(imagePathChannel)); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}()
+
+	for file := range imagePathChannel {
+
+		fullPath := strings.Split(file, string("\\"))
+
+		specificPath := strings.Join(fullPath[1:], "/")
+
+		pathWriter, err := zipWriter.Create(specificPath)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		byteData, err := os.ReadFile(file)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = pathWriter.Write(byteData)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	zipWriter.Close()
+	w.Header().Set("Content-Type", "application/zip")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.zip\"", filename))
+	w.Write(buf.Bytes())
+}
+
+func visit(imagePathChannel chan<- string) filepath.WalkFunc {
+
+	return func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// specifying the paths of files which are of png format
+		if filepath.Ext(path) == ".png" {
+
+			imagePathChannel <- path
+
+		}
+		return nil
+	}
 }
