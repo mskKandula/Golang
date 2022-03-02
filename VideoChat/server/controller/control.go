5f0718fc2135424b331a207cea69db3a545df24c
mskKandula <mskKandula@gmail.com>
2022-03-02 18:02:17 +0530

    JoinRoom Implementation

--- VideoChat/server/controller/control.go
+++ VideoChat/server/controller/control.go
@@ -2,7 +2,19 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	room     Room
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
 )
 
 func CreateRoom(w http.ResponseWriter, r *http.Request) {
@@ -17,5 +29,35 @@ func CreateRoom(w http.ResponseWriter, r *http.Request) {
 }
 
 func JoinRoom(w http.ResponseWriter, r *http.Request) {
+	roomId, ok := r.URL.Query()["roomId"]
+	if !ok {
+		log.Println("roomId missing in URL Parameters")
+		return
+	}
 
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal("Web Socket Upgrade Error", err)
+	}
+
+	room.InsertIntoRoom(roomId[0], ws, false)
+
+	go broadcaster()
+
+	for {
+		var msg broadcastMsg
+
+		err := ws.ReadJSON(&msg.Message)
+
+		if err != nil {
+			log.Fatal("Read Error: ", err)
+		}
+
+		msg.Client = ws
+		msg.RoomId = roomId[0]
+
+		log.Println(msg.Message)
+
+		broadcast <- msg
+	}
 }
