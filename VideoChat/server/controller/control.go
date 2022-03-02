9684c9d568f1b1a6b3aa267f0456bd8bf27b406c
mskKandula <mskKandula@gmail.com>
2022-03-02 17:00:36 +0530

    HandlerFuncs

---
+++ VideoChat/server/controller/control.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func CreateRoom(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	roomID := room.RoomCreation()
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"roomId": roomID,
+	})
+}
+
+func JoinRoom(w http.ResponseWriter, r *http.Request) {
+
+}
