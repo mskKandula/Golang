ae25e45c7f9ca737a519831768cf82957747a235
mskKandula <mskKandula@gmail.com>
2022-03-11 15:00:37 +0530

    SSE Initialized

---
+++ SSE/V2/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	counter     int
+	clients     map[string]*Client
+	clientsList []string
+)
+
+type (
+	Client struct {
+		name   string
+		events chan *DashBoard
+	}
+	Currency float64
+	Item     struct {
+		Name     string   `json:"name,omitempty"`
+		Quantity int      `json:"quantity,omitempty"`
+		Price    Currency `json:"price,omitempty"`
+	}
+	Store struct {
+		Items map[string]Item `json:"items,omitempty"`
+	}
+	DashBoard struct {
+		Users         uint       `json:"users,omitempty"`
+		UsersLoggedIn uint       `json:"users_logged_in,omitempty"`
+		Inventory     *Store     `json:"inventory,omitempty"`
+		ChartOne      []int      `json:"chart_one,omitempty"`
+		ChartTwo      []Currency `json:"chart_two,omitempty"`
+	}
+)
+
+func main() {
+	fmt.Println("Hello")
+	clients = make(map[string]*Client)
+
+	// register static files handle '/index.html -> client/index.html'
+	// http.Handle("/", http.FileServer(http.Dir("client")))
+	// register RESTful handler for '/sse/dashboard'
+	http.HandleFunc("/sse/dashboard", dashbaordHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func dashbaordHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Client: %v", r.RemoteAddr)
+	client := clients[r.RemoteAddr]
+	if nil == client {
+		client = addClient(r.RemoteAddr)
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case ev := <-client.events:
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.Encode(ev)
+		fmt.Fprintf(w, "data: %v\n\n", buf.String())
+		fmt.Printf("data: %v\n", buf.String())
+	case <-timeout:
+		fmt.Fprintf(w, ": nothing to sent\n\n")
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
