09a6d651d2514acf93ad94c35daf91f436bdc1be
mohanak <mohanak@mkcl.org>
2022-05-07 17:02:28 +0530

    Stream handler Implementation

--- VideoStreaming/HLS/server/main.go
+++ VideoStreaming/HLS/server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -14,9 +16,29 @@ func main() {
 func handlers() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", indexPage).Methods("GET")
+	router.HandleFunc("/media/{mId:[0-9]+}/", streamHandler).Methods("GET")
 	return router
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../client/index.html")
+}
+
+func streamHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	mId, err := strconv.Atoi(vars["mId"])
+	if err != nil {
+		response.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	mediaBase := getMediaBase(mId)
+
+	fmt.Println(mediaBase)
+
+}
+
+func getMediaBase(mId int) string {
+	mediaRoot := "../assets/media"
+	return fmt.Sprintf("%s/%d", mediaRoot, mId)
 }
