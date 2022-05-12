63e7f0b039a7c8df8f4938bc45dad48186df11b2
mohanak <mohanak@mkcl.org>
2022-05-12 18:03:19 +0530

    Reading Words from File

--- Trie/main.go
+++ Trie/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
 const (
 	//ALBHABET_SIZE total characters in english alphabet
 	ALBHABET_SIZE = 26
@@ -28,5 +35,29 @@ func main() {
 }
 
 func (t *trie) readFileAndInsert(fileName string) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0755)
 
+	if err != nil {
+		log.Fatalln("Error while opening the file: ", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// To read word by word
+	// scanner.Split(bufio.ScanWords)
+
+	// To read line by line
+	for scanner.Scan() {
+		lineText := scanner.Text()
+		// insert after converting to lowercase
+		t.insert(strings.ToLower(lineText))
+	}
+
+	if err = scanner.Err(); err != nil {
+		log.Fatalln("Error while reading file: ", err)
+	}
 }
+
+func (t *trie) insert(word string) {}
