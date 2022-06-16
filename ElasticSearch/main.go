1546e980f3bc03bd1a4d1479679b45821e9a20e2
mohanak <mohanak@mkcl.org>
2022-06-16 15:32:11 +0530

    Data Insertion

--- ElasticSearch/main.go
+++ ElasticSearch/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 
 	elastic "gopkg.in/olivere/elastic.v7"
 )
 
+type Student struct {
+	Name         string  `json:"name"`
+	Age          int64   `json:"age"`
+	AverageScore float64 `json:"averageScore"`
+}
+
 func main() {
-	_, err := GetESClient()
+	ctx := context.Background()
+
+	esclient, err := GetESClient()
 	if err != nil {
 		fmt.Println("Error initializing : ", err)
 		panic("Client fail ")
 	}
+
+	//creating student object
+	newStudent := Student{
+		Name:         "ABC",
+		Age:          18,
+		AverageScore: 75.5,
+	}
+
+	dataJSON, err := json.Marshal(newStudent)
+	js := string(dataJSON)
+	_, err = esclient.Index().
+		Index("students").
+		BodyJson(js).
+		Do(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("[Elastic][InsertProduct]Insertion Successful")
 }
 
 func GetESClient() (*elastic.Client, error) {
