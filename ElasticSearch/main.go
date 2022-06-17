689972ed644dce34c5cfa7e584988dbf6f13cb13
mohanak <mohanak@mkcl.org>
2022-06-17 12:24:33 +0530

    Querying Data

--- ElasticSearch/main.go
+++ ElasticSearch/main.go
@@ -42,6 +42,16 @@ func main() {
 	}
 
 	fmt.Println("[Elastic][InsertProduct]Insertion Successful")
+
+	searchResults, err := DataSearch(esclient, ctx, "ABC")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, s := range searchResults {
+		fmt.Printf("Student found Name: %s, Age: %d, Score: %f \n", s.Name, s.Age, s.AverageScore)
+	}
+
 }
 
 func GetESClient() (*elastic.Client, error) {
@@ -54,4 +64,50 @@ func GetESClient() (*elastic.Client, error) {
 
 	return client, err
 
+}
+
+func DataSearch(esclient *elastic.Client, ctx context.Context, searchString string) ([]Student, err) {
+
+	var students []Student
+
+	searchSource := elastic.NewSearchSource()
+	searchSource.Query(elastic.NewMatchQuery("name", searchString))
+
+	/* this block will basically print out the es query */
+	queryStr, err1 := searchSource.Source()
+	if err1 != nil {
+		fmt.Println("[esclient][GetResponse]err during query marshal=", err1)
+		return nil, err1
+	}
+
+	queryJs, err2 := json.Marshal(queryStr)
+
+	if err2 != nil {
+		fmt.Println("[esclient][GetResponse]err during query marshal=", err2)
+		return nil, err2
+	}
+
+	fmt.Println("[esclient]Final ESQuery=\n", string(queryJs))
+	/* until this block */
+
+	searchService := esclient.Search().Index("students").SearchSource(searchSource)
+
+	searchResult, err := searchService.Do(ctx)
+	if err != nil {
+		fmt.Println("[ProductsES][GetPIds]Error=", err)
+		return nil, err
+	}
+
+	for _, hit := range searchResult.Hits.Hits {
+		var student Student
+		err := json.Unmarshal(hit.Source, &student)
+		if err != nil {
+			fmt.Println("[Getting Students][Unmarshal] Err=", err)
+			return nil, err
+		}
+
+		students = append(students, student)
+	}
+
+	return students, nil
 }
