c3d62d21f436a75a4fe17d9d41f30c70888558d9
mohanak <mskKandula@gmail.com>
2022-10-10 15:41:28 +0530

    Insert & Inspect

--- DS&Algo/LinkedList/NewsFeed/main.go
+++ DS&Algo/LinkedList/NewsFeed/main.go
@@ -51,25 +51,68 @@ func (f *Feed) Remove(publishedDate int64) {
 	f.length--
 }
 
+func (f *Feed) Insert(newPost *Post) {
+	if f.length == 0 {
+		f.start = newPost
+		f.end = newPost
+	} else {
+		var previousPost *Post
+		currentPost := f.start
+
+		for currentPost.publishedDate < newPost.publishedDate {
+			previousPost = currentPost
+			currentPost = currentPost.next
+		}
+
+		previousPost.next = newPost
+		newPost.next = currentPost
+
+	}
+	f.length += 1
+}
+
+func (f *Feed) Inspect() {
+	if f.length == 0 {
+		fmt.Println("Feed is empty")
+	}
+	fmt.Println("========================")
+	fmt.Println("Feed Length: ", f.length)
+
+	var currentIndex int8 = 0
+	currentPost := f.start
+
+	for currentIndex < f.length {
+		fmt.Printf("Item: %v - %v\n", currentIndex, currentPost)
+		currentPost = currentPost.next
+		currentIndex++
+	}
+	fmt.Println("========================")
+}
+
 func main() {
+	rightNow := time.Now().Unix()
+
 	f := &Feed{}
 
 	p1 := Post{
 		body:          "First Post()",
-		publishedDate: time.Now().Unix(),
+		publishedDate: rightNow,
 	}
 	f.Append(&p1)
 
-	fmt.Printf("Length: %v\n", f.length)
-	fmt.Printf("First: %v\n", f.start)
-
 	p2 := Post{
 		body:          "Second Post",
-		publishedDate: time.Now().Unix(),
+		publishedDate: rightNow + 20,
 	}
 	f.Append(&p2)
 
-	fmt.Printf("Length: %v\n", f.length)
-	fmt.Printf("First: %v\n", f.start)
-	fmt.Printf("Second: %v\n", f.start.next)
+	f.Inspect()
+
+	p3 := &Post{
+		body:          "This is a new post",
+		publishedDate: rightNow + 15,
+	}
+	f.Insert(p3)
+
+	f.Inspect()
 }
