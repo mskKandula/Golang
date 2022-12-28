5da79dc33252ff82dd8b33f5e37ccfa7dfd13efc
mskKandula <mskKandula@gmail.com>
2022-12-28 17:11:05 +0530

    gobwas Client

---
+++ Gobwas/Client/client.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+func main() {
+	fmt.Println("Client started")
+
+	for {
+		conn, _, _, err := ws.DefaultDialer.Dial(context.Background(), "ws://127.0.0.1:9000/")
+		if err != nil {
+			fmt.Println("Cannot connect: " + err.Error())
+			time.Sleep(time.Duration(5) * time.Second)
+			continue
+		}
+
+		fmt.Println("Connected to server")
+
+		for i := 0; i < 10; i++ {
+			randomNumber := strconv.Itoa(rand.Intn(100))
+			msg := []byte(randomNumber)
+			err = wsutil.WriteClientMessage(conn, ws.OpText, msg)
+			if err != nil {
+				fmt.Println("Cannot send: " + err.Error())
+				continue
+			}
+
+			fmt.Println("Client message send with random number " + randomNumber)
+
+			msg, _, err := wsutil.ReadServerData(conn)
+			if err != nil {
+				fmt.Println("Cannot receive data: " + err.Error())
+				continue
+			}
+
+			fmt.Println("Server message received with random number: " + string(msg))
+			time.Sleep(time.Duration(5) * time.Second)
+		}
+
+		err = conn.Close()
+		if err != nil {
+			fmt.Println("Cannot close the connection: " + err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Disconnected from server")
+	}
+}
