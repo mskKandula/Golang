4ac2810f2a1c019aa840ba4204854bb126c2bc5f
mskKandula <mskKandula@gmail.com>
2023-05-30 14:32:43 +0530

    SQL, Docker & Compose Files

---
+++ CRUD/SPH/MySQL/sph.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Articles`(
+    id INT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`)
+);
