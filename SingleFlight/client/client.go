ce1f1b9d0a7d1775a1d85448377c6cf8e9fe2df6
mskKandula <mskKandula@gmail.com>
2023-10-27 15:29:07 +0530

    Client to test SingleFlight

---
+++ SingleFlight/client/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	endpoint := "http://localhost:8080/api/v1/get_something?name=something"
+	totalRequests := 5
+
+	for i := 0; i < totalRequests; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			makeAPICall(endpoint)
+		}(i)
+	}
+	wg.Wait()
+
+}
+
+func makeAPICall(endpoint string) {
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	result := string(body)
+	log.Printf(result)
+}
